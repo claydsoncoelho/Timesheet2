5deb8205413f324a1bd78623e69a2a0edc002473
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-02-08 15:39:39 +1300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ def main():
     end_time = st.time_input("End Time")
     task = st.text_input("Task Description")
 
-    if start_time and end_time and task:
-        duration = end_time - start_time
-        st.write("Duration: ", duration)
-        st.write("Task: ", task)
-
     if st.button("Submit"):
         with open("timesheet.txt", "a") as f:
             f.write(f"{start_time} - {end_time}: {task}\n")
