e6da0262da6371cd9d4043d7b4e69f06358a30d3
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-02-09 14:47:12 +1300

    Update main.py

--- main.py
+++ main.py
@@ -87,6 +87,6 @@ with tab3:
                 df = df[~df.index.isin([i for i, row in df.iterrows() if not(selected_rows[i])])]
                 selected_rows = [False] * len(df)
                 st.write("Data:")
-                for _, row in df.interrows():
+                for _, row in df.iterrows():
                         msg = delete_resource(row["Name"])
                 st.success(msg, icon="✅")
