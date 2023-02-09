80a2d5c6ccef4151aa6527a70c3b56c400edcd26
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-02-09 14:50:14 +1300

    Update main.py

--- main.py
+++ main.py
@@ -90,4 +90,3 @@ with tab3:
                 for _, row in df.iterrows():
                         msg = delete_resource(row["Name"])
                 st.write('')
-                st.success(msg, icon="✅")
