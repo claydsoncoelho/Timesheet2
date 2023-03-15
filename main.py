cee75eb383612d8297be6086585990fa2f3f28ad
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-03-16 11:02:46 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-import time
 
 tab1, tab2, tab3 = st.tabs(["Time Entry", "Reports", "Team"])
 
@@ -20,63 +17,27 @@ def get_all_resources():
                                 "Rate": [123]
                         }
                 )
-
         return my_data
 
 
-def insert_resource(name, rate):
-        new_row = {'Name': name, 'Rate': rate}
-        df = get_all_resources()
-        df = df.append(new_row, ignore_index=True)
-        df.to_csv("timesheet.txt", index=False, sep="\t")
-
-
-def delete_resource(name):
-        my_data = pd.read_csv("timesheet.txt", sep="\t")
-        index_to_delete = my_data[my_data['Name'] == name].index
-        my_data.drop(index_to_delete, inplace=True)
-        my_data.to_csv("timesheet.txt", index=False, sep="\t")
-        return "Resources deleted."
+def save_resource(df):
+        df.to_csv("timesheet.txt", index=False, sep="\t")       
         
-        
-def refresh_gid():
-        
-        return df;
 
         
 with tab1:
         st.header("A cat")
         st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-        
-        with open("timesheet.txt", "r") as f:
-            data = f.read()
-            st.write(data)
 
 with tab2:
         st.header("A dog")
         st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
 with tab3:
-        st.header("Team")
-        name = st.text_input("Name", value="")
-        rate = st.number_input("Rate", value=0.00)
 
-        save_button = st.button("Save member")
-        if save_button:
-                if name and rate:
-                        insert_resource(name, rate)
-                        st.success('Saved', icon="✅")
-       
         df = get_all_resources()
                                       
         # Display the DataFrame with checkboxes
         edited_df = st.experimental_data_editor(df, num_rows="dynamic")
-                        
-        delete_button = st.button("Delete member")
-        if delete_button:
-                df = df[~df.index.isin([i for i, row in df.iterrows() if not(selected_rows[i])])]
-                selected_rows = [False] * len(df)
-                for _, row in df.iterrows():
-                        msg = delete_resource(row["Name"])
-                st.write('')
-                st.success('Deleted', icon="✅")
+        
+        save_resources(edited_df)
