a7275975257f2b3b7ae748b0b6577a0e2aa8783b
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-03-16 11:07:15 +1300

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,6 @@ with tab3:
         # Display the DataFrame with checkboxes
         edited_df = st.experimental_data_editor(df, num_rows="dynamic")
         
-        save_resources(edited_df)
+        if save_button:
+                save_resources(edited_df)
+                st.success('Saved', icon="✅")
